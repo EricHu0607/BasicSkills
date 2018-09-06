1775e1e5ab1d77cdfc141a7c1e3c3661609d6605
huchenxuan <965774357@qq.com>
2018-09-06 23:32:29 +0800

    LeetCode 62 Unique Paths

---
+++ cppCode/leetcode62.cpp
@@ -0,0 +1,57 @@
+/*
+62. Unique Paths
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Above is a 7 x 3 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+*/
+
+/*
+分析：此题可以用动态规划来求解。i行j列有以下策略：
+		1）i == 0 ,j == 0 为初始条件，dp[i][0] = 1;dp[0][j] = 1
+		2）i != 0 && j != 0:dp[i][j] = dp[i][j-1]+dp[i-1][j];
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+		if(m == 0 || n == 0)
+            return 0;
+		vector<vector<int>> dp(m,vector<int>(n));
+		for(int i = 0; i < m; i++)
+			dp[i][0] = 1;
+		for(int i = 0; i < n; i++)
+			dp[0][i] = 1;
+		for(int i = 1; i < m; i++)
+		{
+			for(int j = 1; j < n; j++)
+			{
+				dp[i][j] = dp[i-1][j] + dp[i][j-1];
+			}
+		}
+		return dp[m-1][n-1];
+    }
+};
